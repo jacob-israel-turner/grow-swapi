0a209939845eadbcb1586f9105d304a2e321bc14
Jacob Turner <jacob.turner@domo.com>
2017-03-10 23:06:02 -0700

    Refactor/organize sort code

--- services/swapi.js
+++ services/swapi.js
@@ -17,27 +17,35 @@ async function getCharacters({limit, start, sort, asc}) {
   await cachePassword
   if (!characterSortFields.includes(sort)) throw new Error(`'${sort}' is not a valid sort field`)
   let sortedPeopleCache
-  if (sort) sortedPeopleCache = peopleCache
-    .sort((a, b) => {
-      let aVal = a[sort]
-      let bVal = b[sort]
-      if (aVal === bVal || isNaN(aVal) && isNaN(bVal)) {
-        if (a.name > b.name) return up
-        else return down
-      }
-      if (sort === 'height' || sort === 'mass') {
-        aVal = Number(aVal.replace(',', ''))
-        bVal = Number(bVal.replace(',', ''))
-        if (isNaN(bVal)) return up;
-        if (isNaN(aVal)) return down;
-      }
-      let isBigger = aVal > bVal
-      if (isBigger) return up
-      else return down
-    })
+  if (sort) sortedPeopleCache = peopleCache.sort(handleSort)
   else sortedPeopleCache = peopleCache
   return sortedPeopleCache
     .slice(start - 1, start + limit - 1)
+
+  function handleSort(a, b) {
+    const aVal = a[sort]
+    const bVal = b[sort]
+    if (aVal === bVal || isNaN(aVal) && isNaN(bVal)) return secondarySort(a, b)
+    else if (sort === 'height' || sort === 'mass') return numericSort(a, b)
+    else return stringSort(a, b)
+  }
+
+  function stringSort(a, b) {
+    if (a[sort] > b[sort]) return up
+    else return down
+  }
+
+  function numericSort(a, b) {
+    const aVal = Number(a[sort].replace(',', ''))
+    const bVal = Number(b[sort].replace(',', ''))
+    if (isNaN(bVal) || aVal > bVal) return up;
+    else  return down;
+  }
+
+  function secondarySort(a, b) {
+    if (a.name > b.name) return up
+    else return down
+  }
 }
 
 function establishCache() {
